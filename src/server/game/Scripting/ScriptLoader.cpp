039390c7c80133096f31f4382af501843ea19915
Cypisct <exoeler@gmail.com>
2015-12-30 19:56:04 +0100

    Update ScriptLoader.cpp

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1413,6 +1413,7 @@ void AddBattlegroundScripts()
 /* This is where custom scripts' loading functions should be declared. */
 
 void AddSC_NPC_TransmogDisplayVendor();
+void AddSC_Npc_Beastmaster();
 #endif
 
 void AddCustomScripts()
@@ -1421,5 +1422,6 @@ void AddCustomScripts()
     /* This is where custom scripts should be added. */
 
     AddSC_NPC_TransmogDisplayVendor();
+    AddSC_Npc_Beastmaster();
 #endif
 }
